e7cf5dc0783f9145a499d86c7d57d1b528f659e5
aditwhorra42 <aditwhorra@gmail.com>
2021-04-03 03:20:31 +0530

    Removed dummy data and pulled data from db

--- blog/views.py
+++ blog/views.py
@@ -1,28 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from blog.models import Post
 
 # Create your views here.
 
-posts = [
-    {
-        "author": "Adit",
-        "title": "First Blog",
-        "content": "This is the first blog",
-        "date_posted": "01/01/2021"
-    },
-    {
-        "author": "Adit",
-        "title": "Second Blog",
-        "content": "This is the second blog",
-        "date_posted": "01/03/2021"
-    }
-]
-
 def home_view(request):
-    context = {
-        "posts": posts,
-        "title": "Home"
-    }
+    context = {"posts": Post.objects.all(), "title": "Home"}
     return render(request, 'blog/home.html', context)
 
 def about_view(request):
