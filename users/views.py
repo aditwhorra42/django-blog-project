4d3eb5157b709701deb6257ff9ade19ce0916314
aditwhorra42 <aditwhorra@gmail.com>
2021-04-04 22:00:07 +0530

    Added user registration and login views

---
+++ users/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from .forms import UserRegistrationForm
+from django.contrib import messages
+
+def register(request):
+    if request.method == "POST":
+        form = UserRegistrationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f"Account has been successfully created for {username}")
+            form.save()
+            return redirect('blog-home')
+    else:
+        form = UserRegistrationForm()
+    return render(request, 'users/register.html', {'form': form})
+
