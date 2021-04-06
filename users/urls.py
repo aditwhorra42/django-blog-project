612029bcb9431a0bfcb4ee912c7ee14eae0014b3
aditwhorra42 <aditwhorra@gmail.com>
2021-04-06 23:42:28 +0530

    Added user profile

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/', views.register, name='register-user'),
     path('profile/', views.profile, name='user-profile')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
